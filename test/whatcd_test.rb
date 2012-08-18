0fcf0734a38d8c6a051c5c2c3aa6fb8f702066e6
Paul Brickfeld <paulbrickfeld@gmail.com>
2012-08-18 22:22:01 +0200

    Fixed a bug where requests without parameters would produce an error

--- test/whatcd_test.rb
+++ test/whatcd_test.rb
@@ -13,12 +13,19 @@ context WhatCD do
 		hookup { WhatCD::authenticate username, password }
 
 		asserts('is authenticated') { topic::authenticated? }
-		asserts('can make requests') { topic::User(id: 28747) }.kind_of Hash
-		asserts('raises exceptions') { topic::Gibberish }.raises NameError
-		asserts('supports rippy') { topic::Rippy }.kind_of String
+		asserts('requests with parameters') { 
+			topic::User(id: 28747)
+		}.kind_of Hash
+		asserts('requests without paramters') { topic::Index }.kind_of Hash
+		asserts('raises exceptions') { topic::Xxx }.raises WhatCD::APIError
 	end
 
 	context 'Incorrect credentials' do
-		asserts('raises AuthError') { WhatCD::authenticate '.', '..' }.raises WhatCD::AuthError
+		asserts('raises AuthError') {
+			topic::authenticate '.', '..'
+		}.raises WhatCD::AuthError
+		asserts('raises AuthError') {
+			topic::User(id: 28747)
+		}.raises WhatCD::AuthError
 	end
 end
