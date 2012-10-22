c6ddaa0a0e86a38dbf89a0f055f3666a0686ba56
Paul Brickfeld <paulbrickfeld@gmail.com>
2012-10-22 17:19:45 +0200

    Readability

--- test/whatcd_test.rb
+++ test/whatcd_test.rb
@@ -10,22 +10,18 @@ context WhatCD do
 	asserts('includes HTTParty methods') { topic }.kind_of? HTTParty
 
 	context 'Correct credentials' do
-		hookup { WhatCD::authenticate username, password }
+		hookup { topic::authenticate username, password }
 
 		asserts('is authenticated') { topic::authenticated? }
-		asserts('requests with parameters') { 
-			topic::User(id: 28747)
-		}.kind_of Hash
+		asserts('requests with parameters') {  topic::User(id: 28747) }.kind_of Hash
 		asserts('requests without paramters') { topic::Index }.kind_of Hash
 		asserts('raises exceptions') { topic::Xxx }.raises WhatCD::APIError
 	end
 
 	context 'Incorrect credentials' do
-		asserts('raises AuthError') {
-			topic::authenticate '.', '..'
-		}.raises WhatCD::AuthError
-		asserts('raises AuthError') {
-			topic::User(id: 28747)
-		}.raises WhatCD::AuthError
+		hookup { topic::cookies {} }
+		
+		asserts('raises AuthError') { topic::authenticate '.', '..' }.raises WhatCD::AuthError
+		asserts('raises AuthError') { topic::User(id: 28747) }.raises WhatCD::AuthError
 	end
 end
